bf754708c2b07d830ee494f8af6578cb077240bd
20CS074Mandar <20cs074@charusat.edu.in>
2022-09-13 21:24:03 +0530

    Variables and functions

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+
+    //diference between var and val
+    var a=10
+    println(a)
+    val thisIs :Int
+    thisIs=10
+    println(thisIs)
+    //thisIs=10
+    println(thisIs)
+
+    val count:Int
+    //print(count)
+    count=10
+    print(count)
+    //count=11
+    //count=12
+
+}
+
+fun calculator()
+{
+
+}
